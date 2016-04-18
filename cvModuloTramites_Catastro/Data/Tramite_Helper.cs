2a0501bd6961289764c434a35179b3d6d8619b93
FerNHK <fer_nhk@hotmail.com>
2016-04-18 01:29:18 -0500

    Tramite_Helper
    *Creacion de Clase de Tramite_Heleper

---
+++ cvModuloTramites_Catastro/Data/Tramite_Helper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cvModuloTramites_Catastro.Data
+{
+    public class Tramite_Helper
+    {
+    }
+}
